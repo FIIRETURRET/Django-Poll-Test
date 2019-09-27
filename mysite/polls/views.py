cea2b3c82111b0414f248aeac67400c2bf3922f3
FIIRETURRET <brandonjoutras@gmail.com>
2019-09-27 17:26:08 -0500

    Add files via upload

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,14 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from django.template import loader
+from django.http import Http404
+
+from .models import Question
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls indx.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)                     #Returns HttpResponse object of the given template rendered with the given context.
     
     
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." %question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})     #Replaces the try catch clause to find a 404 error. If a object is not returned the 404 is returned instead.
     
     
 def results(request, question_id):
